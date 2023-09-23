7eab42974c8ccf11b19b9f8d510c99b57eaf58c3
Ahmed Raza <raza1778@gmail.com>
2023-09-24 01:21:12 +0500

    API Auth, user and posts endpoints

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,19 @@
 from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView
+)
 from .views import (
-    home_view
+    home_view,
+    user_view,
+    register_view,
+    posts_view
 )
 
 urlpatterns = [
-    path('', home_view, name='home')
+    path('', home_view, name='home'),
+    path('api/login', TokenObtainPairView.as_view(), name='api-login'),
+    path('api/register', register_view, name='api-register'),
+    path('api/user', user_view, name='api-user'),
+    path('api/posts', posts_view, name='api-posts')
 ]
