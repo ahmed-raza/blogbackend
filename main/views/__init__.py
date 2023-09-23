37d733c88dd38a50a8d4a16b16298eb7e9113733
Ahmed Raza <raza1778@gmail.com>
2023-09-24 01:22:02 +0500

    Views returing

--- main/views/__init__.py
+++ main/views/__init__.py
@@ -1,6 +1,11 @@
 from main.views.HomeView import home_view
-
+from main.views.api.UserView import user_view
+from main.views.api.RegisterView import register_view
+from main.views.api.PostsView import posts_view
 
 __all__ = [
-    "home_view"
+    "home_view",
+    "user_view",
+    "register_view",
+    "posts_view"
 ]
