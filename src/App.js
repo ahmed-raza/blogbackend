219b65813f5b2b970c1c2b8e0a1054fab5072934
Ahmed Raza <raza1778@gmail.com>
2023-09-24 13:48:10 +0500

    Defined react routes

--- src/App.js
+++ src/App.js
@@ -1,93 +1,25 @@
-import { useEffect, useState } from "react";
-import "./App.css";
-import { Container, Header, Menu, MenuItem, Modal } from "semantic-ui-react";
-import { getPosts } from "./utils/posts";
-import Posts from "./components/Posts";
-import Register from "./components/auth/Register";
-import { getAccessToken, removeAccessToken } from "./utils/auth";
-import { getUser } from "./utils/user";
-import Login from "./components/auth/Login";
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Main from "./components/Main";
+import Profile from "./components/user/Profile";
+import { checkAuthLoader, getAccessToken } from "./utils/auth";
+import RootLayout from "./components/RootLayout";
 
-function App() {
-  const [accessToken, setAccessToken] = useState(getAccessToken());
-  const [user, setUser] = useState({});
-  const [posts, setPosts] = useState([]);
-  const [open, setOpen] = useState(false);
-  const [mode, setMode] = useState("login");
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    id: "root",
+    loader: getAccessToken,
+    children: [
+      { path: "/", element: <Main /> },
+      { path: "/my-account", element: <Profile />, loader: checkAuthLoader },
+    ],
+  },
+]);
 
-  useEffect(() => {
-    getPosts().then((data) => setPosts(data));
-    if (accessToken) {
-      getUser().then((data) => setUser(data));
-    }
-  }, [accessToken]);
-
-  const handleLogout = () => {
-    removeAccessToken();
-    window.location.reload();
-  };
-
-  return (
-    <>
-      <Menu>
-        <Container>
-          {accessToken ? (
-            <>
-              <MenuItem key={1} link={true}>
-                {user.username}
-              </MenuItem>
-              <MenuItem key={2} link={true} onClick={handleLogout}>
-                Logout
-              </MenuItem>
-            </>
-          ) : (
-            <>
-              <MenuItem
-                key={1}
-                link={true}
-                onClick={() => {
-                  setOpen(true);
-                  setMode("login");
-                }}
-              >
-                Login
-              </MenuItem>
-              <MenuItem
-                key={2}
-                link={true}
-                onClick={() => {
-                  setOpen(true);
-                  setMode("register");
-                }}
-              >
-                Register
-              </MenuItem>
-            </>
-          )}
-        </Container>
-      </Menu>
-      <Container text>
-        <Header as="h2">Welcome to the Blog</Header>
-        {posts && <Posts posts={posts}></Posts>}
-      </Container>
-      <Modal
-        onClose={() => setOpen(false)}
-        onOpen={() => setOpen(true)}
-        open={open}
-      >
-        <Modal.Header>
-          {mode === "login" ? "Login" : "Register Account"}
-        </Modal.Header>
-        <Modal.Content>
-          {mode === "login" ? (
-            <Login onLogin={(access) => setAccessToken(access)} />
-          ) : (
-            <Register onRegister={(user) => setUser(user)} />
-          )}
-        </Modal.Content>
-      </Modal>
-    </>
-  );
-}
+const App = () => {
+  return <RouterProvider router={router} />;
+};
 
 export default App;
