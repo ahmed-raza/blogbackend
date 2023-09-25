f4ccc76b0d7486c2fac37d79742eb2cb9ce4c17e
Ahmed Raza <raza1778@gmail.com>
2023-09-25 12:24:20 +0500

    User update view

---
+++ main/views/api/UserUpdateView.py
@@ -0,0 +1,27 @@
+from django.contrib.auth.models import User
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from ...serializers.UserSerializer import UserSerializer
+
+class UpdateUserView(APIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = UserSerializer
+    def put(self, request, pk=None):
+        try:
+            user = User.objects.get(id=pk)
+        except User.DoesNotExist:
+            user = None
+
+        if user:
+            serializer = self.serializer_class(user, request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response({'message': 'Yolo'})
+            else:
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            return Response({'error': 'User does not exist.'}, status=status.HTTP_404_NOT_FOUND)
+
+update_user_view = UpdateUserView.as_view()
