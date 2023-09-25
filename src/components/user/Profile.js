ac49a34b6c74d6800b14c419be08ebf9f0c3f57a
Ahmed Raza <raza1778@gmail.com>
2023-09-25 12:25:50 +0500

    User profile form

--- src/components/user/Profile.js
+++ src/components/user/Profile.js
@@ -1,10 +1,82 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
+import { Button, Form, Header, Input } from "semantic-ui-react";
+import { getUser, updateUser } from "../../utils/user";
 
 const Profile = () => {
+  const testRef = useRef();
+  const [user_id, setUserId] = useState("");
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [first_name, setFirstName] = useState("");
+  const [last_name, setLastName] = useState("");
+  const [errors, setErrors] = useState([]);
+
+  useEffect(() => {
+    getUser().then((data) => {
+      setUserId(data.id);
+      setUsername(data.username);
+      setEmail(data.email);
+      setFirstName(data.first_name);
+      setLastName(data.last_name);
+    });
+  }, []);
+
+  const handleUpdate = (e) => {
+    e.preventDefault();
+    setErrors([]);
+    const formData = new FormData();
+    formData.append("username", username);
+    formData.append("first_name", first_name);
+    formData.append("last_name", last_name);
+    formData.append("email", email);
+    updateUser(user_id, formData).then(({ data, status }) => {
+      if (status === 400 || status === 401) {
+        setErrors(data);
+      }
+    });
+  };
+
   return (
-    <div>
-      <h1>My Account</h1>
-    </div>
+    <>
+      <Header as="h1">My Account</Header>
+      <Form ref={testRef}>
+        <Form.Field
+          error={errors.username}
+          control={Input}
+          label="Username"
+          className="six wide"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <Form.Field
+          error={errors.email}
+          control={Input}
+          label="Email"
+          className="six wide"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <Form.Field
+          error={errors.first_name}
+          control={Input}
+          label="First Name"
+          className="six wide"
+          value={first_name}
+          onChange={(e) => setFirstName(e.target.value)}
+        />
+        <Form.Field
+          error={errors.last_name}
+          control={Input}
+          label="First Name"
+          className="six wide"
+          value={last_name}
+          onChange={(e) => setLastName(e.target.value)}
+        />
+        <Button type="submit" className="olive basic" onClick={handleUpdate}>
+          Update
+        </Button>
+      </Form>
+    </>
   );
 };
 
