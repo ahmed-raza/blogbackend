521abdbf10aa928305129fbcd775854c31157f87
Ahmed Raza <raza1778@gmail.com>
2023-09-25 18:48:28 +0500

    Add image field to Post

--- main/models.py
+++ main/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 class Post(models.Model):
     title = models.CharField(max_length=255, null=False, blank=False)
     body = models.TextField()
+    image = models.ImageField(upload_to='uploads/', null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     def __str__(self):
