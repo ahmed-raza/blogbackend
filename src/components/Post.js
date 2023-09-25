1cc7e5eceb2c1c21fd6d354c7eb235c5b87ebca5
Ahmed Raza <raza1778@gmail.com>
2023-09-25 19:12:27 +0500

    Render post image

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,11 +1,14 @@
 import React from "react";
-import { Card, Icon } from "semantic-ui-react";
+import { Card, Icon, Image } from "semantic-ui-react";
 
 const Post = ({ post }) => {
   return (
     <Card>
-      <Card.Content header={post.title} />
-      <Card.Content description={post.short_description} />
+      <Image src={process.env.REACT_APP_API_BASE_URL + post.image} wrapped />
+      <Card.Content>
+        <Card.Header>{post.title}</Card.Header>
+        <Card.Description>{post.short_description}</Card.Description>
+      </Card.Content>
       <Card.Content extra>
         <p>
           <Icon name="user"></Icon>
